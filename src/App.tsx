bfe7b6b1ded2de26ae58da296765494107eba677
hibiki hangai <hibihibi99@gmail.com>
2024-10-09 20:15:06 +0900

    todoアプリ作成

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,64 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [text, setText] = useState<string>("");
+  const [todo, setTodo] = useState<string[]>([]);
+  const [compTodo, setCompTodo] = useState<string[]>([]);
+
+  // 追加
+  const onClickAdd = () => {
+    setTodo([...todo, text]);
+    setText("");
+  };
+
+  // 完了
+  const onClickComplete = (text: string) => {
+    setCompTodo([...compTodo, text]);
+    const upDateTodo = todo.filter((todo) => todo !== text);
+    setTodo(upDateTodo);
+  };
+
+  // 削除
+  const onClickDelete = (text: string) => {
+    const upDateTodo = todo.filter((todo) => todo !== text);
+    setTodo(upDateTodo);
+  };
+
+  // 戻す
+  const onClickReturn = (text: string) => {
+    const upDateTodo = compTodo.filter((todo) => todo !== text);
+    setCompTodo(upDateTodo);
+    setTodo([...todo, text]);
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>Todoアプリ</p>
+      <div className="addTodo">
+        <input value={text} onChange={(e) => setText(e.target.value)} />
+        <button onClick={() => onClickAdd}>追加</button>
+      </div>
+      <div className="incompleteContainer">
+        <p>未完了</p>
+        {todo.map((text, index) => (
+          <ul key={index}>
+            <li>
+              {index + 1} : {text}
+            </li>
+            <button onClick={() => onClickComplete(text)}>完了</button>
+            <button onClick={() => onClickDelete(text)}>削除</button>
+          </ul>
+        ))}
+      </div>
+      <div className="completeContainer">
+        <p>完了</p>
+        {compTodo.map((text, index) => (
+          <ul>
+            <li>{text}</li>
+            <button onClick={() => onClickReturn(text)}>戻す</button>
+          </ul>
+        ))}
+      </div>
     </div>
   );
 }
---
+++ src/component/addTodo.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const addTodo = () => {
+  <ul>
+    <li>todo2</li>
+    <button>完了</button>
+    <button>削除</button>
+  </ul>;
+};
